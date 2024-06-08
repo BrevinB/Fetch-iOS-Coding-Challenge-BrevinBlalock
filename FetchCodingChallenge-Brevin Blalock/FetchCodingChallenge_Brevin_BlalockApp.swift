5b550e9d7f3558da5d2019d9c624bc91a99d5fe0
BrevinB <bblalock313@gmail.com>
2024-06-08 09:57:10 -0400

    created xcode project

---
+++ FetchCodingChallenge-Brevin Blalock/FetchCodingChallenge_Brevin_BlalockApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchCodingChallenge_Brevin_BlalockApp.swift
+//  FetchCodingChallenge-Brevin Blalock
+//
+//  Created by Brevin Blalock on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchCodingChallenge_Brevin_BlalockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
